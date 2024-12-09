c57557819593daaee62ab8b8afa8fcffa56a07f5
merge-simpson <letsdev@kakao.com>
2024-12-09 14:57:08 +0900

    refactor: move DemoApplication from me.nettee.demo to me.nettee

--- src/main/java/me/nettee/DemoApplication.java
+++ src/main/java/me/nettee/DemoApplication.java
@@ -1,9 +1,11 @@
-package me.nettee.demo;
+package me.nettee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import static me.nettee.common.constant.Constants.ROOT_PACKAGE;
+
+@SpringBootApplication(scanBasePackages = ROOT_PACKAGE)
 public class DemoApplication {
 
     public static void main(String[] args) {
