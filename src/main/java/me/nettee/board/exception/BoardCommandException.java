c46da44b9fcc506652af232cb03219b5b0919895
merge-simpson <letsdev@kakao.com>
2024-12-10 16:43:13 +0900

    feat(board): add enum board command error code and its exception(shorten)

---
+++ src/main/java/me/nettee/board/exception/BoardCommandException.java
@@ -0,0 +1,18 @@
+package me.nettee.board.exception;
+
+import me.nettee.common.exeption.CustomException;
+import me.nettee.common.exeption.ErrorCode;
+
+public class BoardCommandException extends CustomException {
+    // (intellij) Ctrl + O
+    // (eclipse or sts) Alt Shift S => generate constructors
+
+
+    public BoardCommandException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+
+    public BoardCommandException(ErrorCode errorCode, Throwable cause) {
+        super(errorCode, cause);
+    }
+}
