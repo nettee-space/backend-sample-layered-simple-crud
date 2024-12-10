76bd876288a7e8752cda1e57227915d7ef7d748f
merge-simpson <letsdev@kakao.com>
2024-12-10 14:28:54 +0900

    feat(board): add response status codes to the creation api

--- src/main/java/me/nettee/board/controller/BoardCommandApi.java
+++ src/main/java/me/nettee/board/controller/BoardCommandApi.java
@@ -7,9 +7,11 @@ import me.nettee.board.controller.dto.BoardCommandDto.BoardCreateResponse;
 import me.nettee.board.usecase.BoardCreateUseCase;
 import me.nettee.board.usecase.BoardDeleteUseCase;
 import me.nettee.board.usecase.BoardUpdateUseCase;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,6 +23,7 @@ public class BoardCommandApi {
     private final BoardDeleteUseCase boardDeleteUseCase;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public BoardCreateResponse create(
             @RequestBody @Valid BoardCreateRequest request
     ) {
@@ -30,4 +33,19 @@ public class BoardCommandApi {
                 .board(board)
                 .build();
     }
+
+//    @PostMapping
+//    public ResponseEntity<BoardCreateResponse> create2(
+//            @RequestBody @Valid BoardCreateRequest request
+//    ) {
+//        var board = boardCreateUseCase.create(request);
+//
+//        var dto =  BoardCreateResponse.builder()
+//                .board(board)
+//                .build();
+//
+//        return ResponseEntity
+//                .status(HttpStatus.CREATED)
+//                .body(dto);
+//    }
 }
